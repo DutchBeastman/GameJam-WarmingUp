9f2a6d94cbf237ca93b14d7fe4f56c42edc353c8
DutchBeastman <Fabianverk@hotmail.com>
2014-08-26 16:10:25 +0200

    Light and Building

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,26 +2,26 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-	public float movementSpeed = 10;
+	public float movementSpeed = 100;
 	void Start(){
 		//Transform.position = new Vector3 (0, 3, 0);
 	}
 	void Update () {
 		if (Input.GetKey ("w")) {
-						rigidbody.AddRelativeForce (0, 100, 0);
-				} else {
-						rigidbody.velocity = new Vector3 (0, 0, 0);
-				}
+			rigidbody.AddRelativeForce (0, 0, movementSpeed);
+		} else {
+			rigidbody.velocity = new Vector3 (0, 0, 0);
+		}
 		if (Input.GetKey ("s")) {
-			rigidbody.AddRelativeForce(0,-100,0);				
+			rigidbody.AddRelativeForce(0,0,-movementSpeed);				
 		} else {
 			rigidbody.velocity = new Vector3 (0, 0, 0);
 		}
 		if (Input.GetKey ("d")) {
-			transform.Rotate(0,0,-3);
+			transform.Rotate(0,5,0);
 }
 		if (Input.GetKey ("a")) {
-			transform.Rotate(0,0,3);
+			transform.Rotate(0,-5,0);
 }
 }
 }
