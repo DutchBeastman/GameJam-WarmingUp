166ff63b0507f6150cd99d549200360da9ac5675
DutchBeastman <Fabianverk@hotmail.com>
2014-08-26 12:29:59 +0200

    FlashLight

---
+++ Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerActions : MonoBehaviour {
+	public Light zaklamp;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F))
+		{
+			if(zaklamp.enabled == false)
+			{
+				zaklamp.enabled = true;
+			}
+			else if (zaklamp.enabled == true)
+			{
+				zaklamp.enabled = false;
+			}
+		}
+	
+	}
+}
