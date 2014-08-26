57a22d79865c0c6f1e1698c0281d51de645a3181
Djamali Shawn Jones <15546@ma-web.nl>
2014-08-26 11:40:37 +0200

    Camera Follow & Movement
    Camera Follow en voorlopige Movement toegevoegd

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothTime = 0.25f;
+
+	private Vector3 velocity = Vector3.zero;
+	
+	void Update() 
+	{
+		Vector3 goalPos = target.position;
+		goalPos.y = transform.position.y;
+		transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, smoothTime);
+	}
+}
