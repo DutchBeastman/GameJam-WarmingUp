7c187fb136b256efa2e4ab4117bd69cd64e9f9ed
DutchBeastman <Fabianverk@hotmail.com>
2014-08-27 10:20:55 +0200

    Pushing Objects

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class PlayerActions : MonoBehaviour {
 	public Light zaklamp;
+	public float pushPower = 2.0F;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,7 +12,8 @@ public class PlayerActions : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+			
+		
 		if(Input.GetKeyDown(KeyCode.F))
 		{
 			if(zaklamp.enabled == false)
@@ -23,5 +26,16 @@ public class PlayerActions : MonoBehaviour {
 			}
 		}
 	
+	}
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		Rigidbody body = hit.collider.attachedRigidbody;
+		if (body == null || body.isKinematic)
+			return;
+		
+		if (hit.moveDirection.y < -0.3F)
+			return;
+		
+		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+		body.velocity = pushDir * pushPower;
 	}
 }
