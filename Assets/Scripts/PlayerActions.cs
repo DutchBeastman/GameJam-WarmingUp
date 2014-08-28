3d0f4436f9c49b5090959be9ee1af137de19e90e
DutchBeastman <Fabianverk@hotmail.com>
2014-08-28 09:11:49 +0200

    Fixed errors

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -34,22 +34,15 @@ public class PlayerActions : MonoBehaviour {
 				if (hit.collider.name == "MoveableObject") {
 						Rigidbody body = hit.collider.attachedRigidbody;
 						if (body == null || body.isKinematic)
-								return;
-		if(hit.collider.name == "MoveableObject"){
-		Rigidbody body = hit.collider.attachedRigidbody;
-		if (body == null || body.isKinematic)
-			return;
+						return;
 		
 						if (hit.moveDirection.y < -0.3F)
-								return;
+						return;
 		
 						Vector3 pushDir = new Vector3 (hit.moveDirection.x, 0, hit.moveDirection.z);
 						body.velocity = pushDir * pushPower;
 				}
-		}
-		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-		body.velocity = pushDir * pushPower;
-		}
+
 	}
 
 
