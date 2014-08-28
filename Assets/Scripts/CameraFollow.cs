3e1c8b59a93d293c8576ad4fa29b329c83fcac26
DutchBeastman <Fabianverk@hotmail.com>
2014-08-28 09:04:03 +0200

    update camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,13 +5,22 @@ public class CameraFollow : MonoBehaviour {
 
 	public Transform target;
 	public float smoothTime = 0.55f;
-
+	private float playerY;
+	private float playerZ;
 	private Vector3 velocity = Vector3.zero;
-	
+	private Vector3 positionPlayer;
+	void start(){
+		//playerY = target.position.y + 4;
+		//playerZ = target.position.z - 3;
+		//positionPlayer = new Vector3(-3f ,playerY,playerZ);
+	}
 	void Update() 
 	{
+
 		Vector3 goalPos = target.position;
 		goalPos.y = transform.position.y;
+		//goalPos.z = transform.position.z -3;
+		//goalPos.x = transform.position.x - 3;
 		transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, smoothTime);
 	}
 }
